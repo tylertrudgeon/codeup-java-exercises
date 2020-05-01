19c7635d4530ed27e2aed1d87b6e8dab7110b53e
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-01 12:06:30 -0500

    first problem completed in exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,6 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f.", pi);
+    }
+}
