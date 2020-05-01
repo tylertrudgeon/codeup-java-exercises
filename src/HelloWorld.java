476612b65bf5651c4bb1e9d3a9eec2e9f1e3bcef
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-01 09:45:09 -0500

    finished up to second part of 13 on syntax, types, and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,13 +13,27 @@ public class HelloWorld {
         System.out.println(myNumber);
 
         //this one increments before the line is printed where x++ increments after
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
 //        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//
+//        System.out.println(x);
+
+        int x = 3;
+        int y = 4;
+        y = y * x;
+
+
+
+
+
 
 
 
