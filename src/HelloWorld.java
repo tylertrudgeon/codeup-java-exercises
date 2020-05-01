ff1b6aeacfeb65c90599c5be42dc640af8d76a99
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-01 09:35:00 -0500

    finished up to 11 on syntax, types, and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,5 +12,10 @@ public class HelloWorld {
         float myNumber = 3.14f;
         System.out.println(myNumber);
 
+        //this one increments before the line is printed where x++ increments after
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
     }
 }
