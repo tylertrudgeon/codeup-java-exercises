744b3f3e02283724e12a0c38eb4a907d0ef8be61
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-01 09:26:47 -0500

    finished second pard of syntax, types, and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.print("World!" + "\n");
+
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Tyler";
+        System.out.println(myString);
+
+
     }
 }
