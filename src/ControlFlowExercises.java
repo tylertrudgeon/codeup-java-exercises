1af36210d51d7bcc7810498808ab8c44992b2ca8
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-04 12:04:29 -0500

    finished 2nd exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,26 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        int i = 5;
-//
-//        while(i <= 15) {
-//            System.out.println("i = " + i);
-//            i++;
-//        }
-
-//        int i = 2;
-//        do {
-//            System.out.println("i = " + (i * i));
-//            i++;
-//        } while (i < 1000000);
-
-        for (int i = 5; i <= 15; i++) {
-            System.out.println("i = " + i);
-        }
-
-        for (int i = 2; i <= 1000000; i++) {
-            i = i * i;
-            System.out.println("i = " + i);
+        for (int i = 1; i <= 100; i++) {
+            if ( i % 3 == 0 && i % 5 == 0 ) {
+                System.out.println("FizzBuzz");
+            } else if ( i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if ( i % 5 == 0) {
+                System.out.println("Buzz");
+            }
         }
 
     }
