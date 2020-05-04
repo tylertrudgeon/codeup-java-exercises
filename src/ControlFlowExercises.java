f8b6ec1bb5dba043b3b45509cd2587c08654e85e
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-04 12:41:04 -0500

    finished 3rd

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-            if ( i % 3 == 0 && i % 5 == 0 ) {
-                System.out.println("FizzBuzz");
-            } else if ( i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if ( i % 5 == 0) {
-                System.out.println("Buzz");
-            }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What number would you like to go up to?");
+        int number = scanner.nextInt();
+
+        System.out.println("Here is your table!");
+        System.out.println();
+
+        String topOfTable = "number  |" + "  squared  |" + "  cubed\n"
+                          + "--------|" + "-----------|" + "-------";
+        System.out.println(topOfTable);
+
+        String row = "";
+        int square = 0;
+        int cubed = 0;
+
+        for (int i = 1; i <= number; i++) {
+            square = i * i;
+            cubed = i * i * i;
+
+            row = i + "       |" + square + "          " + "|" + cubed;
+
+            System.out.println(row);
         }
 
     }
