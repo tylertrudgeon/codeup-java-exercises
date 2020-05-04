c8eae61fc279f42927bde6c6cccc1305c965ebe0
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-04 11:23:09 -0500

    finished first part of 1b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,20 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
+//        int i = 5;
+//
+//        while(i <= 15) {
+//            System.out.println("i = " + i);
+//            i++;
+//        }
 
-        while(i <= 15) {
-            System.out.println("i = " + i);
+        int i = 0;
+        do {
+            if (i % 2 == 0) {
+                System.out.println("i = " + i);
+            }
             i++;
-        }
+        } while (i <= 100);
 
     }
 }
