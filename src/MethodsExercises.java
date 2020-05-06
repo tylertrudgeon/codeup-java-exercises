a03e14dcb7fdf7b2723d8e98fecd60737d48ad6c
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-06 10:00:13 -0500

    finished 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+
     public static void main(String[] args) {
 
         System.out.println(add(2, 5));
         System.out.println(subtract(10, 4));
-        System.out.println(multiply(6, 5));
+        System.out.println(multiply(6, 3));
         System.out.println(divide(10, 2));
         System.out.println(modulus(8,2));
+        getInteger(1, 10);
 
     }
 
@@ -32,5 +36,20 @@ public class MethodsExercises {
 
     public static int modulus(int num1, int num2) {
         return num1 % num2;
+    }
+
+    public static void getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1 and 10: ");
+        int num = scanner.nextInt();
+
+        if (num > 0 && num < 10 ) {
+            System.out.println("That is a valid number.");
+            return;
+        }
+
+        System.out.println("That is not a valid number, try again.");
+        getInteger(min, max);
     }
 }
